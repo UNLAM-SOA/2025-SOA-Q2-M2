4b3f8c4281abbcc1598e6dc4a0fd7778b55936a2
Lu <lu_qp@hotmail.com>
2025-11-05 03:27:16 -0300

    MQTT - Medidor de consumo

--- Android/AguaSmart/app/src/main/java/com/example/aguasmart/ConsumoActivity.java
+++ Android/AguaSmart/app/src/main/java/com/example/aguasmart/ConsumoActivity.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Random;
+import com.github.anastr.speedviewlib.DeluxeSpeedView;
+import com.github.anastr.speedviewlib.SpeedView;
+import com.github.anastr.speedviewlib.components.Section;
+
 
 public class ConsumoActivity extends AppCompatActivity {
 
@@ -22,12 +26,29 @@ public class ConsumoActivity extends AppCompatActivity {
     private Button btnActualizar;
     private BroadcastReceiver consumoReceiver;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumo);
 
         tvConsumo = findViewById(R.id.tvConsumo);
+
+        SpeedView gauge = findViewById(R.id.gaugeConsumo);
+
+        // Limpiar secciones por si vienen por defecto
+
+        gauge.setSpeedometerWidth(100);
+        gauge.setWithTremble(false);
+
+
+        // Establecer zonas (0-100 ejemplo)
+        gauge.clearSections();
+        gauge.addSections(new Section(0f, 0.25f, Color.parseColor("#4CAF50")));   // Verde
+        gauge.addSections(new Section(0.25f, 0.50f, Color.parseColor("#FFEB3B"))); // Amarillo
+        gauge.addSections(new Section(0.50f, 0.75f, Color.parseColor("#FF9800"))); // Naranja
+        gauge.addSections(new Section(0.75f, 1f,   Color.parseColor("#F44336")));  // Rojo 100f, Color.parseColor("#F44336")));  // Rojo
 
         // Botón volver
         findViewById(R.id.btnVolverMenu).setOnClickListener(v -> {
@@ -49,6 +70,16 @@ public class ConsumoActivity extends AppCompatActivity {
                 if (message == null || !message.startsWith("CONSUMO:")) return;
 
                 String valor = message.replace("CONSUMO:", "").trim();
+                float consumoFloat = 0f;
+                try {
+                    consumoFloat = Float.parseFloat(valor);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+                // Actualizar el medidor
+
+                gauge.speedTo(consumoFloat, 0);
 
                 // Actualiza UI
                 tvConsumo.setText(String.format(" %s L", valor));
